255dce3c2652a7fc472dcc229fe6e3e3ae1972e6
xiexie <2947171285@qq.com>
2024-02-15 20:13:14 +0800

    封装了svg-icon组件

---
+++ src/libs/svg-icon/index.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  // icon 图标name
+  name: {
+    type: String,
+    required: true
+  },
+  // icon 图标颜色
+  color: {
+    type: String
+  },
+  // icon 图标的 tailwind css样式
+  fillClass: {
+    type: String
+  }
+
+})
+
+// 得到真正的 icon 图标name (拼接上 #icon-)
+const svgIconId = computed(() => `#icon-${props.name}`)
+</script>
+
+<template>
+  <svg aria-hidden="true">
+    <use :xlink:href="svgIconId" :fill="color" :class="fillClass"></use>
+  </svg>
+</template>
+
+<style lang='scss' scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import './styles/index.scss'
 import router from '@/router/index.js'
 import { useREM } from '@/utils/flexible'
+import useLibs from '@/libs/index.js'
+
+// 注册 svg-icons
+import 'virtual:svg-icons-register'
 const app = createApp(App)
 app.use(router)
+app.use(useLibs)
 useREM()
 app.mount('#app')
