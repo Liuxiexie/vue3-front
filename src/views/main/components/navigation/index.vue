f48750745fd22fd64a6c8a3ad584437bb947786a
xiexie <2947171285@qq.com>
2024-02-15 21:25:49 +0800

    给navigation新增了一个item项

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -3,12 +3,14 @@ import { isMobileTerminal } from '@/utils/flexible';
 import mobileNavigation from './mobile/index.vue'
 import { ref } from 'vue'
 import { getCategory } from '@/api/category';
+import { ALL_CATEGORY_ITEM } from '@/constants/index.js'
 
 // 获取所有类目
 const categoryData = ref()
 const getCategoryData = async () => {
   const { categorys } = await getCategory()
   categoryData.value = categorys
+  categoryData.value.unshift(ALL_CATEGORY_ITEM)
 }
 getCategoryData()
 </script>
