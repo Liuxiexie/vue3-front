f2220417910f918eb6c6143202ae80198b15220a
xiexie <2947171285@qq.com>
2024-02-15 14:44:34 +0800

    配置了响应拦截器以简化数据获取流程

--- src/utils/request.js
+++ src/utils/request.js
@@ -11,3 +11,17 @@ service.interceptors.request.use((config) => {
   return config // 必须返回配置
 })
 export default service
+
+/**
+ * 响应拦截器:
+ * 在服务端返回数据之后, 前端.then之前会被调用
+ */
+service.interceptors.response.use((response) => {
+  const { data, message, success } = response.data
+  if (success) {
+    return data
+  } else {
+    // TODO: 业务错误
+    return Promise.reject(new Error(message))
+  }
+})
