b574313fef570e3e9fa39b7d6f61287bda188283
xiexie <2947171285@qq.com>
2024-02-16 14:33:10 +0800

    处理pc端的基础架构

---
+++ src/views/layout/components/floating.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div class="fixed bottom-10 right-2">Floating</div>
+</template>
+
+<style lang='scss' scoped>
+</style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import headerVue from './components/header/index.vue'
+import mainVue from './components/main.vue'
+import floatingVue from './components/floating.vue'
+</script>
+
+<template>
+  <header-vue class="h-header"></header-vue>
+  <main-vue class="h-main"></main-vue>
+  <floating-vue></floating-vue>
+</template>
+
+<style lang='scss' scoped>
+</style>
