99083101068839ee1e5744f9bbd81ccd6cfba440
xiexie <2947171285@qq.com>
2024-02-16 13:00:43 +0800

    使用useVModel实现双向数据绑定

--- src/libs/popup/index.vue
+++ src/libs/popup/index.vue
@@ -1,14 +1,16 @@
 <script setup>
-import { useScrollLock } from '@vueuse/core';
+import { useScrollLock, useVModel } from '@vueuse/core';
 import { watch } from 'vue'
 const props = defineProps({
   modelValue: {
     type: Boolean
   }
 })
-const emits = defineEmits(['update:modelValue'])
+defineEmits(['update:modelValue'])
+// 这是一个响应式数据, 当 isVisible 发生变化时, 自动触发 emit 修改 modelValue
+const isVisible = useVModel(props)
 const isLocked = useScrollLock(document.body)
-watch(() => { props.modelValue }, (val) => {
+watch(isVisible, (val) => {
   isLocked.value = val
 })
 </script>
@@ -18,13 +20,12 @@ watch(() => { props.modelValue }, (val) => {
     <teleport to='body'>
       <!-- 蒙板 -->
       <transition name="fade">
-        <div v-if="modelValue" @click="emits('update:modelValue')"
-          class="w-screen h-screen bg-zinc-900/80 z-30 fixed top-0 left-0">我是蒙蔽
+        <div v-if="isVisible" @click="isVisible = false" class="w-screen h-screen bg-zinc-900/80 z-30 fixed top-0 left-0">
         </div>
       </transition>
       <!-- 弹出层 -->
       <transition name="popup-down-up">
-        <div v-if="modelValue" v-bind="$attrs" class="w-screen bg-white z-50 fixed bottom-0">我是弹出层</div>
+        <div v-if="isVisible" v-bind="$attrs" class="w-screen bg-white z-50 fixed bottom-0">我是弹出层</div>
       </transition>
     </teleport>
   </div>
