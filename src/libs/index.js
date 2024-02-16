e5fc8249538706751541960c3713baf32b9f932f
xiexie <2947171285@qq.com>
2024-02-16 13:23:45 +0800

    实现vite通用组件自动化注册

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,9 +1,17 @@
-import svgIcon from './svg-icon/index.vue'
-import popup from './popup/index.vue'
+import { defineAsyncComponent } from 'vue'
 
 export default {
   install(app) {
-    app.component('m-svg-icon', svgIcon)
-    app.component('m-popup', popup)
+    // 1. 获取当前路径之下的所有 index.vue
+    const components = import.meta.glob('./*/index.vue')
+    // 2. 遍历获取到的组件模块
+    for (const [fullPath, fn] of Object.entries(components)) {
+      // 获取组件名称
+      const componentName = 'm-' + fullPath.replace('./', '').split('/')[0]
+      // 取得异步组件
+      const component = defineAsyncComponent(fn)
+      app.component(componentName, component)
+    }
+    // 3. 使用 app.component 注册组件
   }
 }
